41022a519c0b4430e34665aa7562e8ad83163c19
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-05-14 14:28:53 +0530

    update

---
+++ day2.py
@@ -0,0 +1,18 @@
+name="Harsh"
+print("Name = "+name)
+
+firstname="Virat"
+lastname="Kohli"
+print(firstname+" "+lastname)
+
+name1="Amit"
+course="python"
+fees=120000
+print("Student Details")
+print("Name "+name1)
+print("course "+course)
+
+#---------------Convert integer to string---------------------
+
+print("fees "+str(fees))
+
