46d3f2be98e12267b2f604e83011b88428484e13
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-05-15 14:46:36 +0530

    update

---
+++ day3.py
@@ -0,0 +1,50 @@
+#------------------Checking datatype of variable----------------------
+
+
+# name="Harsh"
+# age=22
+# salary=15000.01
+# employed=True
+# print(type(name))
+# print(type(age))
+# print(type(salary))
+# print(type(employed))
+
+
+#-------------------f-function to print strings--------------------------
+
+
+# print("-------Employe Details---------")
+# print(f"Employe name : {name}")
+# print(f"Employe age : {age}")
+# print(f"Employe salary : {salary}")
+# print(f"Is employed : {employed}")
+
+
+#--------------------Taking input from user-------------------------------
+
+
+# name=input("Enter your name : ")
+# age=int(input("Enetr your age : "))
+# salary=float(input("Enter your salary : "))
+# employed=bool(input("Enter employed true or false : "))
+
+# print(f"Employe name : {name}")
+# print(f"Employe age : {age}")
+# print(f"Employe salary : {salary}")
+# print(f"Is employed : {employed}")
+
+
+# write a program to accept rollno, name, course, and fees and display it
+
+name=input("Enter your name : ")
+roll=int(input("Enter your roll no : "))
+course=input("Enter your course : ")
+fees=float(input("Enter your fees : "))
+
+print(f"Student name : {name}")
+print(f"Student roll no : {roll}")
+print(f"Student cousre : {course}")
+print(f"Student fees : {fees}")
+
+
