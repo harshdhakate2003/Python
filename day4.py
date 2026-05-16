8f764d697eb8387c3c11f6cca14a0c9eebf48f2f
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-05-16 17:05:48 +0530

    update

---
+++ day4.py
@@ -0,0 +1,31 @@
+# print("Enter employee details : ")
+# empno=int(input("Enter the employee number : "))
+# name=input("Enter name : ")
+# salary=int(input("Enter the salary : "))
+# da=salary*0.45
+# ta=salary*0.85
+# hra=salary*0.35
+# total=salary+da+ta+hra
+# print(f"Employe number : {empno}")
+# print(f"Employee name : {name}")
+# print(f"da : {da}")
+# print(f"ta : {ta}")
+# print(f"hra : {hra}")
+# print(f"total salary : {total}")
+
+
+# write a program to accept length and breath and print area of trangle
+
+length=int(input("Enter the length : "))
+breath=int(input("Enter the breath : "))
+area=length*breath
+print(f"Area = {area}")
+
+
+# write a program to accept radius and print area and curcumferance of circle
+
+radius=int(input("Enter radius : "))
+area=3.14*radius*radius
+circ=2+3.14+radius
+print(f"Area = {area}")
+print(f"circ = {circ}")
