6c7326f51f8dbdc4dba4a8b7043a8cb2c51d0635
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-05-19 20:47:53 +0530

    update

---
+++ day5.py
@@ -0,0 +1,8 @@
+sub1=int(input("Enter marks 1 : "))
+sub2=int(input("Enter marks 2 : "))
+sub3=int(input("Enter marks 3 : "))
+total=sub1+sub2+sub3
+percent=(total/300)*100
+print(f"total marks = {total}")
+print(f"percentage = {percent}")
+
