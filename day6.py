44cd52bc4288b30b040ab80591d8a04647730324
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-05-22 09:45:20 +0530

    update

---
+++ day6.py
@@ -0,0 +1,109 @@
+# n=2
+# result=n**4
+# print(result)
+# print(type(n))
+
+# num=2.4
+# print(type(num))
+
+# name="Itprenuer"
+# print(type(name))
+
+# choice=True
+# print(type(choice))
+
+# ch="h"
+# print(type(ch))
+
+#--------------------------------------------------
+
+
+# name=input("Enter the name : ")
+# age=int(input("Enter the age : "))
+# salary=float(input("Enter the salary : "))
+# regular=bool(input("Enter true or false for regular employee : "))
+# print(f"Name of Employee : {name} , Age : {age} , Salary : {salary} , Regular : {regular}")
+
+
+#-------------------------------------------------------
+
+# write a program to accept age and print whether eligible to vote or not
+# if else means conditional statements or control flow statements
+
+# age=int(input("Enter your age : "))
+# if (age>=18):
+#     print("You are eiligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+
+#-------------------------------------------------------------
+
+# WAP to accept 2 numbers and print greater number
+
+# n1=int(input("Enter number1 : "))
+# n2=int(input("Enter number2 : "))
+# if(n1>n2):
+#     print(f"{n1} is greater")
+# else:
+#     print(f"{n2} is greater")
+
+
+#-------------------------------------------------------------
+
+# WAP to accept 3 numbers and print greater number
+
+# n1=int(input("Enter number1 : "))
+# n2=int(input("Enter number2 : "))
+# n3=int(input("Enter number3 : "))
+
+# if n1>n2 and n1>n3:
+#     print(f"{n1} is greater")
+# elif n2>n3:
+#     print(f"{n2} is greater")
+# else:
+#     print(f"{n3} is greater")
+
+
+#------------------------Home Work-------------------------
+
+# WAP to accept 3 subjects and print the total, percentage and grade
+
+# sub1=int(input("Enter a marks of sub1 : "))
+# sub2=int(input("Enter a marks of sub2 : "))
+# sub3=int(input("Enter a marks of sub3 : "))
+
+# total=sub1+sub2+sub3
+# percent=(total/300)*100
+# print(f"Total marks : {total}")
+# print(f"Percent : {percent}")
+
+# if percent>=90:
+#     print("A grade")
+# elif percent>=80:
+#     print("B grade")
+# elif percent>=70:
+#     print("C grade")
+# else:
+#     print("D grade")
+
+
+#----------------------------------------------------------------
+
+# WAP to accept temperature and print whether hot, cold or normal
+
+# temp=int(input("Enter the temperature : "))
+
+# if temp>=35:
+#     print("Hot")
+# elif temp<=15:
+#     print("Cold")
+# else:
+#     print("Normal")
+
+
+#-------------------------------------------------------------------
+
+# WAP to accept salary and designation calculate bonus 15% for manager,
+# 12% for clerk and 5% for peon of salary and print total salary.
+
