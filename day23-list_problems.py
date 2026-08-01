58fb06cdc41d57677e07bf60f45dee35d574f236
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-08-01 19:39:52 +0530

    update

--- day23-list_problems.py
+++ day23-list_problems.py
@@ -22,15 +22,6 @@
 #         lst.reverse()
 
 
-#--------------------------------------------------------------------
-
-# n = int(input("Enter the number of compititer : "))
-# arr = list(map(int, input().split()))
-# max_score = max(arr)
-# runner_up = max(x for x in arr if x != max_score)
-# print(runner_up)
-
-
 
 
 
